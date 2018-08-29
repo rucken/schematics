74a6cc9d3e4b271a11bfc0fde2d074d0ceeb8ea9
EndyKaufman <admin@site15.ru>
2018-08-29 14:13:23 +0500

    chore: Update source

--- fixtures/nestjs/new/ormconfig.js
+++ fixtures/nestjs/new/ormconfig.js
@@ -1,9 +1,28 @@
 const ConnectionString = require('connection-string').ConnectionString;
-const config = require('dotenv').config;
+const load = require('dotenv').load;
 const path = require('path');
 const fs = require('fs');
+const NODE_ENV = process.env.NODE_ENV || 'develop';
+// todo: wait resolve https://github.com/typeorm/typeorm/issues/2358
+// const dbSourceExt = NODE_ENV === 'develop' ? 'ts' : 'js';
+// const sourceRootKey = NODE_ENV === 'develop' ? 'sourceRoot' : 'outputPath';
+const dbSourceExt = 'ts';
+const sourceRootKey = 'sourceRoot';
 const nestCliConfig = JSON.parse(fs.readFileSync('.nestcli.json'));
-config();
+try {
+    fs.accessSync(`${NODE_ENV}.env`);
+    load({ path: `${NODE_ENV}.env` });
+    console.log(`env file: ${NODE_ENV}.env`);
+} catch (error) {
+    console.log(`error on get env file: ${NODE_ENV}.env`);
+    try {
+        fs.accessSync(`.env`);
+        load();
+        console.log(`env file: .env`);
+    } catch (error) {
+        console.log(`error on get env file: .env`);
+    }
+}
 
 const connectionString = new ConnectionString(process.env.DATABASE_URL);
 const projects = Object.keys(nestCliConfig.projects).map(key => nestCliConfig.projects[key]);
@@ -13,33 +32,37 @@ const defaultProject = nestCliConfig.defaultProject;
 const defaultApp = nestCliConfig.projects[defaultProject];
 
 if (connectionString.protocol === 'sqlite') {
+    const databaseUrl =
+        (dbSourceExt === 'ts' ? './' : '../') +
+        connectionString.hosts[0].name +
+        (connectionString.path.length ? '/' + connectionString.path[0] : '');
     module.exports = {
         type: 'sqlite',
-        database: './' + connectionString.hosts[0].name + (connectionString.path.length ? '/' + connectionString.path[0] : ''),
-        entities: projects.map(lib => `${lib.sourceRoot}/**/entities/**/*.entity.ts`),
-        migrations: projects.map(lib => `${lib.sourceRoot}/**/migrations/**/*.ts`),
-        subscribers: projects.map(lib => `${lib.sourceRoot}/**/subscribers/**/*.ts`),
+        database: databaseUrl,
+        entities: [...libs, ...apps].map(lib => `${lib[sourceRootKey]}/**/entities/**/*.entity.${dbSourceExt}`),
+        migrations: [...libs, ...apps].map(lib => `${lib[sourceRootKey]}/**/migrations/**/*.${dbSourceExt}`),
+        subscribers: [...libs, ...apps].map(lib => `${lib[sourceRootKey]}/**/subscribers/**/*.${dbSourceExt}`),
         logging: 'all',
         synchronize: false,
         cli: {
-            migrationsDir: `${defaultApp.sourceRoot}/migrations`
+            migrationsDir: `${defaultApp[sourceRootKey]}/migrations`
         }
     }
 } else {
     module.exports = {
         type: connectionString.protocol,
-        host: connectionString.host,
-        port: +connectionString.port,
+        host: connectionString.hosts[0].name,
+        port: +connectionString.hosts[0].port,
         username: connectionString.user,
         password: connectionString.password,
-        database: connectionString.segments[0],
-        entities: projects.map(lib => `${lib.sourceRoot}/**/entities/**/*.entity.ts`),
-        migrations: projects.map(lib => `${lib.sourceRoot}/**/migrations/**/*.ts`),
-        subscribers: projects.map(lib => `${lib.sourceRoot}/**/subscribers/**/*.ts`),
+        database: connectionString.path[0],
+        entities: [...libs, ...apps].map(lib => `${lib[sourceRootKey]}/**/entities/**/*.entity.${dbSourceExt}`),
+        migrations: [...libs, ...apps].map(lib => `${lib[sourceRootKey]}/**/migrations/**/*.${dbSourceExt}`),
+        subscribers: [...libs, ...apps].map(lib => `${lib[sourceRootKey]}/**/subscribers/**/*.${dbSourceExt}`),
         logging: 'all',
         synchronize: false,
         cli: {
-            migrationsDir: `${defaultApp.sourceRoot}/migrations`
+            migrationsDir: `${defaultApp[sourceRootKey]}/migrations`
         }
     }
 }
--- fixtures/nestjs/new/src/main.hmr.ts
+++ fixtures/nestjs/new/src/main.hmr.ts
@@ -1,3 +1,15 @@
+// tslint:disable-next-line:no-var-requires
+const tsConfig = require('../tsconfig.json');
+// tslint:disable-next-line:no-var-requires no-implicit-dependencies
+const tsConfigPaths = require('tsconfig-paths');
+const NODE_ENV = process.env.NODE_ENV || 'develop';
+if (NODE_ENV !== 'develop') {
+  tsConfigPaths.register({
+    baseUrl: __dirname,
+    paths: tsConfig.compilerOptions.paths
+  });
+}
+
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -21,8 +33,8 @@ import {
   ICoreConfig
 } from '@demo/core-nestjs';
 import { AppModule } from './apps/demo/app.module';
-import { config } from 'dotenv';
-import { accessSync } from 'fs';
+import { load } from 'dotenv';
+import { accessSync, readFileSync } from 'fs';
 import * as path from 'path';
 
 declare const module: any;
@@ -30,19 +42,22 @@ declare const module: any;
 async function bootstrap() {
   const ConnectionString = require('connection-string').ConnectionString;
   const chmod = require('chmod');
-  const packageBody = require('../package.json');
-  const WWW_ROOT = path.resolve(__dirname, '..', 'www');
-  const nodeEnv = process.env.NODE_ENV;
+  const packageBody = JSON.parse(readFileSync('./package.json').toString());
+  const STATIC_FOLDERS = [
+    path.resolve(__dirname, '..', 'www'),
+    path.resolve(__dirname, '..', 'frontend')
+  ];
+
   try {
-    accessSync(`${nodeEnv}.env`);
-    config({ path: `${nodeEnv}.env` });
-    Logger.log(`env file: ${nodeEnv}.env`, 'Main');
+    accessSync(`../${NODE_ENV}.env`);
+    load({ path: `../${NODE_ENV}.env` });
+    Logger.log(`env file: ${NODE_ENV}.env`, 'Main');
   } catch (error) {
     try {
       accessSync(`.env`);
-      config();
+      load();
       Logger.log(`env file: .env`, 'Main');
-    } catch (error) {}
+    } catch (error) { }
   }
   const connectionString = new ConnectionString(process.env.DATABASE_URL);
   if (connectionString.protocol === 'sqlite') {
@@ -52,7 +67,7 @@ async function bootstrap() {
       (connectionString.path.length ? '/' + connectionString.path[0] : '');
     try {
       chmod(dbFile, 777);
-    } catch (error) {}
+    } catch (error) { }
   }
   const coreConfig: ICoreConfig = {
     ...defaultCoreConfig,
@@ -97,7 +112,9 @@ async function bootstrap() {
     }),
     { cors: true }
   );
-  app.useStaticAssets(WWW_ROOT);
+  STATIC_FOLDERS.forEach(folder => {
+    app.useStaticAssets(folder);
+  });
 
   let documentBuilder = new DocumentBuilder()
     .setTitle(packageBody.name)
